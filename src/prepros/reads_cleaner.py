966e9abfec1398d26fe242fcb162da42914f4d18
Riccardo <20014421@studenti.uniupo.it>
2019-05-03 12:17:37 +0200

    refactoring

--- src/prepros/reads_cleaner.py
+++ src/prepros/reads_cleaner.py
@@ -1,37 +1,56 @@
 #!/usr/bin/python3
 
+import os
+import sys
+
 def ascii_from_quality(quality):
-	return chr(min(quality, 93) + 33)
+  return chr(min(quality, 93) + 33)
 
-def reader(quality):
+def reader(quality, reads_file):
 
-	mapIndexSubstring = {}
+  mapIndexSubstring = {}
 
-	with open('data/ngs-reads.fq', 'r') as file:
-		for j, lines in enumerate(file, start = 1):
-			if j % 4 == 0:
-				character = ascii_from_quality(quality)
-				index = [i for i, char in enumerate(lines, start = 0) if char < character or char == character]
-				mapIndexSubstring.update({j: index})
-			else:
-				continue
+  with open(reads_file, 'r') as file:
+    for j, lines in enumerate(file, start = 1):
+      if j % 4 == 0:
+        character = ascii_from_quality(quality)
+        index = [i for i, char in enumerate(lines, start = 0) if char < character]
+        mapIndexSubstring.update({j: index})
 
-	return dict(sorted(mapIndexSubstring.items()))
+  return dict(sorted(mapIndexSubstring.items()))
 
-def cleaner(quality, max_length):
-
-	index = reader(quality)
+def cleaner(quality, max_length, reads_file):
 	
-	with open('data/ngs-reads.fq', 'r') as file, open('out/clean_read.txt', 'w') as writer:
-		for i, lines in enumerate(file, start = 1):
-			if i % 4 == 2:
-				array = index.get(i + 2)
-				count = 0
-				for k in range(0, len(array)):
-					if count != array[k] and len(lines[count : array[k]]) > max_length:
-						writer.write(lines[count : array[k]] + '\n')
+  extension = os.path.splitext(reads_file)[1]
+  if extension != '.fq' and extension != '.fasta':
+    print('Invalid format file')
+  
+  index = reader(quality, reads_file)
 
-					count = array[k] + 1
+  with open(reads_file, 'r') as file, open('out/clean_read.txt', 'w') as writer:
+    for i, lines in enumerate(file, start = 1):
+      if i % 4 == 2:
+        array = index.get(i + 2)
+        count = 0
+        
+        if array[0] == len(lines):
+          writer.write(lines + '\n')
+        else:
+          string = ""
+          for k in range(0, len(array)):
+            if count != array[k] and len(lines[count : array[k]]) > max_length:
+              if len(string) <= len(lines[count : array[k]]):
+                string = lines[count : array[k]]
+                
+            count = array[k] + 1
+          
+          if string == "":
+            print("Characters are all less than input quality\n");
+          else: 
+            writer.write(string + '\n')
 
-	file.close()
-	writer.close()
+  file.close()
+  writer.close()
+
+if __name__ == "__main__":
+  cleaner(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
