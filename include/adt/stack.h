5f6916c307895b22e8f541e8d8a867f29c4addd4
Riccardo <20014421@studenti.uniupo.it>
2019-05-07 21:46:04 +0200

    refactoring

--- include/adt/stack.h
+++ include/adt/stack.h
@@ -8,9 +8,9 @@
 
 	sstack_t stack_create();
 	void stack_destroy(sstack_t stack);
-	void stack_push(sstack_t stack, int data);
-	int stack_pop(sstack_t stack);
-	int stack_top(const sstack_t stack);
+	void stack_push(sstack_t stack, char *data);
+	char* stack_pop(sstack_t stack);
+	char* stack_top(const sstack_t stack);
 	int stack_is_empty(const sstack_t stack);
 	size_t stack_size(const sstack_t stack);
 	void stack_clear(sstack_t stack);
