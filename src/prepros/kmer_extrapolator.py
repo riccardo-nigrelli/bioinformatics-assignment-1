20ba4ac27324559676cbbceaf703ab407791d578
Riccardo <20014421@studenti.uniupo.it>
2019-05-10 12:00:19 +0200

    add exit if is not possible generate kmer

--- src/prepros/kmer_extrapolator.py
+++ src/prepros/kmer_extrapolator.py
@@ -6,6 +6,10 @@ def extrapolator(k):
 
   with open("out/clean_read.txt", 'r') as reader, open("out/kmer.txt", 'w') as writer:
     for line in reader:
+      if len(line.rstrip()) < k:
+        print("ERROR: It's not possible generate k-mer of length " + str(k) + " because the length of reads is " + str(len(line.rstrip())))
+        sys.exit(-1)
+        break
       for i in range(0, len(line.rstrip()) - k + 1):
 	      writer.write(line[i : i + k] + '\n')
 
